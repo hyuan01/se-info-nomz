adeb2789422e816319f910976bf806ae8a776948
Harry Y <79280968+hyuan01@users.noreply.github.com>
2022-04-28 22:25:50 -0400

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -3,29 +3,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from time import sleep
 
 chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
 chrome_options = Options()
-chrome_options.add_extension('src.crx')
 options = [
-    #"--headless",
     "--disable-gpu",
     "--window-size=1920,1200",
     "--ignore-certificate-errors",
-    #"--disable-extensions",
     "--no-sandbox",
-    #"--disable-dev-shm-usage"
 ]
 for option in options:
     chrome_options.add_argument(option)
 
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-driver.get('https://google.com')
-sleep(3)
-print(driver.title)
-driver.get('https://youtube.com')
-sleep(3)
+driver.get('http://youtube.com')
 print(driver.title)
