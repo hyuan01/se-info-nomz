c7a81eb6fa3c8c4facb04b1821489416035fbdce
Harry <hyuan01@bu.edu>
2022-04-28 20:54:02 -0400

    test selenium

--- test_selenium.py
+++ test_selenium.py
@@ -3,8 +3,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from time import sleep
 
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+#chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
 chrome_options = Options()
 chrome_options.add_extension('src.crx')
@@ -13,15 +14,17 @@ options = [
     "--disable-gpu",
     "--window-size=1920,1200",
     "--ignore-certificate-errors",
-    "--disable-extensions",
+    #"--disable-extensions",
     "--no-sandbox",
-    "--disable-dev-shm-usage"
+    #"--disable-dev-shm-usage"
 ]
 for option in options:
     chrome_options.add_argument(option)
 
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+driver = webdriver.Chrome('./chromedriver/chromedriver.exe', options = chrome_options)
 
-driver.get('http://nytimes.com')
+driver.get('https://google.com')
+sleep(3)
+driver.get('https://youtube.com')
+sleep(3)
 print(driver.title)
-input('Press [Enter] To close browser')
